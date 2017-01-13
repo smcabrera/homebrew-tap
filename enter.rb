d1f97cb037ce53b85cbb72e7eb0d09a5a10474fb
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2017-01-13 16:42:10 -0500

    Bump version

--- enter.rb
+++ enter.rb
@@ -6,7 +6,7 @@ class Enter < Formula
   desc "Start hacking on your projects fast. Quickly select among your projects and open a tmux session for it."
   homepage "https://github.com/smcabrera/enter"
   url "https://github.com/smcabrera/enter/archive/1.1.0.tar.gz"
-  version "1.0.0"
+  version "1.1.0"
   sha256 "7fed133a07367fec86dbc13e27a8ed76d9a629ef0ef781fd2d234ca966b9b513"
 
   depends_on "pick"
